45c1f5add1f0149741cf701e3cd2845de2df401d
YaroslavMozharovskiy <ipz222_myas@student.ztu.edu.ua>
2024-01-27 00:18:24 +0200

    c# console + .gitignore

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿for(; ; )
+{
+    int a, b;
+    Console.WriteLine("a = ");
+    a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("b = ");
+    b = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"{a} + {b} = {a+b}");
+}
+
